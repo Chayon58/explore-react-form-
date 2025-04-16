fc60b939b0d2adacf673becf4b668304d70da5ed
Chayon58 <chayonbiswas8610@gmail.com>
2025-04-16 15:53:31 +0600

    added products

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
 
 import './App.css'
+import ControlledField from './components/Controlledfield/Controlledfield'
+import Formaction from './components/Formaction/Formaction'
 import Simpleform from './components/Simpleform/Simpleform'
+import UnControlledField from './components/Uncontrolled/Uncontrolled'
 
 function App() {
 
 
   return (
     <>
-      <h1 className=''>Explore React Form</h1>
-      <Simpleform></Simpleform>
+      <h1>Explore React Form</h1>
+      {/* <Simpleform></Simpleform> */}
+      {/* <Formaction></Formaction> */}
+      <Productmanagement></Productmanagement>
 
-
+      {/* <UnControlledField></UnControlledField> */}
+      {/* <ControlledField></ControlledField> */}
     </>
   )
 }
